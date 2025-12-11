55c58f88716f4bf2b6d2e7eeeddab53ca13d96d5
TopScrech <sergei_saliukov@icloud.com>
2025-12-11 10:15:45 +0100

    -

--- GPU-SortingTests/GPU_SortingTests.swift
+++ GPU-SortingTests/GPU_SortingTests.swift
@@ -1,17 +1,10 @@
-//
-//  GPU_SortingTests.swift
-//  GPU-SortingTests
-//
-//  Created by Sergei Saliukov on 11.12.2025.
-//
-
 import Testing
 @testable import GPU_Sorting
 
 struct GPU_SortingTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    let array = (0...16_000_000).shuffled
+    
+    @Test func testGPUSort() async throws {
+        
     }
-
 }
