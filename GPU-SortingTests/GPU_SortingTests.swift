fd809d92dea5efb626752aab275803c128fa5782
TopScrech <sergei_saliukov@icloud.com>
2025-12-11 10:13:58 +0100

    Initial Commit

---
+++ GPU-SortingTests/GPU_SortingTests.swift
@@ -0,0 +1,17 @@
+//
+//  GPU_SortingTests.swift
+//  GPU-SortingTests
+//
+//  Created by Sergei Saliukov on 11.12.2025.
+//
+
+import Testing
+@testable import GPU_Sorting
+
+struct GPU_SortingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
